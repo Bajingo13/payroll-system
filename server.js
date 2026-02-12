6781ff5521e5759c048c12b36e7a7f616d63dcf8
Arine <jastinearenas2101@gmail.com>
2026-02-12 17:10:59 +0800

    payroll

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ require('./backend/audit_logs')(app, pool);
 require('./backend/utilities')(app, pool);
 
 // ----------- START SERVER -----------
-const PORT = 12687;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
