49d3c2f93ba0c825135289df9c7b9ada8488129e
Arine <jastinearenas2101@gmail.com>
2026-05-14 13:27:38 +0800

    everythig

--- frontend/dashboard/js/script.js
+++ frontend/dashboard/js/script.js
@@ -8710,7 +8710,8 @@ if (window.location.pathname.includes('payroll_journal.html')) {
     if (!list) return;
 
     try {
-      const res = await fetch(`http://127.0.0.1:12687/api/employee_autocomplete?q=${encodeURIComponent(keyword)}`);
+      //const res = await fetch(`http://127.0.0.1:12687/api/employee_autocomplete?q=${encodeURIComponent(keyword)}`);
+      const res = await fetch(`/api/employee_autocomplete?q=${encodeURIComponent(keyword)}`);
       const data = await res.json();
 
       list.innerHTML = "";
